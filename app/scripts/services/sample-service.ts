8238e3e1618427ebf641700d3bff88b8d68a004e
Orta, Jerry <Jerry.Orta@schwab.com>
2015-07-17 17:00:42 -0500

    adding sample service

--- app/scripts/services/sample-service.ts
+++ app/scripts/services/sample-service.ts
@@ -1,3 +1,49 @@
-/**
- * Created by jerryorta-dev on 7/15/15.
- */
+/// <reference path="../../../typings/angularjs/angular.d.ts" />
+
+module myApp.sample {
+
+    //TODO put in different file
+    export interface INumber {
+        base: number;
+        value: number;
+        exponent: number;
+    }
+
+
+    export interface IResult {
+        result: INumber;
+        value: number;
+    }
+
+    class service {
+
+        private $q:ng.IQService;
+        private $http:ng.IHttpService;
+
+        public static $inject = ['$q', '$http',];
+        constructor($q:ng.IQService, $http:ng.IHttpService) {
+            this.$q = $q;
+            this.$http = $http;
+        }
+
+        //TODO what does "static" do?
+        static add(a:INumber, b:INumber): IResult {
+            //var deferred:ng.IDeferred<IResult> = this.$q.defer();
+            var resultant:INumber = <INumber>{
+                base:10,
+                value: 0,
+                exponent: 1
+            };
+
+            resultant.value = a.value + b.value;
+
+            return <IResult>{
+                result: resultant,
+                value: resultant.value
+            }
+        }
+    }
+
+    angular.module('myApp.sample.service', [])
+        .service('sampleService', service)
+}
